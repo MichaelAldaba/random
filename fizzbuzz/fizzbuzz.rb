4df539be6d3ce4a43a0a80c33b9055b4ff98e5e6
Michael Aldaba <maldaba3@gmail.com>
2015-11-06 12:23:15 -0500

    Refactor if/else and add tertiary conditional

--- fizzbuzz/fizzbuzz.rb
+++ fizzbuzz/fizzbuzz.rb
@@ -8,15 +8,10 @@ else puts number
 
 def fizzbuzz
 	(1..100).each do |n|
-		if n % 3 == 0 && n % 5 == 0
-			puts "FizzBuzz"
-		elsif n % 3 == 0
-			puts "Fizz"
-		elsif n % 5 == 0
-			puts "Buzz"
-		else
-			puts n
-		end
+		output = ""
+		output = "Fizz" if n % 3 == 0
+		output += "Buzz" if n % 5 == 0
+		output.length > 0 ? (puts output) : (puts n)
 	end
 end
 
