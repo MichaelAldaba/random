2c4d0c4c01a45244ad19718f810cfec1b9f4d72c
Michael Aldaba <maldaba3@gmail.com>
2015-11-07 00:03:11 -0500

    Remove method and use block

--- fizzbuzz/fizzbuzz.rb
+++ fizzbuzz/fizzbuzz.rb
@@ -6,8 +6,4 @@ if divisible by both, puts fizzbuzz
 else puts number
 =end
 
-def fizzbuzz
-	(1..100).each { |n| n % 3 == 0 ? (n % 5 == 0 ? (puts "FizzBuzz") : (puts "Fizz")) : (n % 5 == 0 ? (puts "Buzz") : (puts n))}
-end
-
-fizzbuzz
+(1..100).each { |n| n % 3 == 0 ? (n % 5 == 0 ? (puts "FizzBuzz") : (puts "Fizz")) : (n % 5 == 0 ? (puts "Buzz") : (puts n))}
