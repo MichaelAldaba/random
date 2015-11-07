b3fe16165594e203931f1a712c4596ee7489bc1d
Michael Aldaba <maldaba3@gmail.com>
2015-11-07 00:13:03 -0500

    Change comments to reflect block

--- fizzbuzz/fizzbuzz.rb
+++ fizzbuzz/fizzbuzz.rb
@@ -1,9 +1,12 @@
 =begin
-iterate from 1-100
-if divisible by 3, puts fizz
-if divisible by 5, puts buzz
-if divisible by both, puts fizzbuzz
-else puts number
+for each number(n) from 1-100
+	is n divisible by 3?
+		if yes, is n also divisible by 5?
+			if yes, puts "FizzBuzz"
+			if no, puts "Fizz"
+		if no, is n divisible by 5?
+			if yes, puts "Buzz"
+			if no, puts n
 =end
 
 (1..100).each { |n| n % 3 == 0 ? (n % 5 == 0 ? (puts "FizzBuzz") : (puts "Fizz")) : (n % 5 == 0 ? (puts "Buzz") : (puts n))}
