3b04e0532d6706bc8aa415084c650dee10b42941
Michael Aldaba <maldaba3@gmail.com>
2016-01-13 14:01:52 -0500

    Make MainMenu class into a Constant

--- tictactoe/second_iteration/game.rb
+++ tictactoe/second_iteration/game.rb
@@ -1,11 +1,19 @@
 require_relative "sub_menu"
 require_relative "main_menu"
 
+MAIN_MENU = { 	
+	1 => SubMenu.new("Go First").name, 
+	2 => SubMenu.new("Go Second").name, 
+	3 => SubMenu.new("Versus").name, 
+	4 => SubMenu.new("Computer Mode").name, 
+	5 => SubMenu.new("Marker Selection").name,
+}
+
 class Game
 	attr_reader :main_menu
 
 	def initialize
-		@main_menu = MainMenu.new.list
+		@main_menu = MAIN_MENU
 		puts "-----------\nTic Tac Toe\n-----------"
 		show_menu(main_menu)
 	end
