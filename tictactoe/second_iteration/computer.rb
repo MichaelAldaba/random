d94dbf3ec7c8125a6e848b69270f8fa24a7df8d3
Michael Aldaba <maldaba3@gmail.com>
2016-01-27 16:30:16 -0500

    Fix glitch of AI not blocking possible forks

--- tictactoe/second_iteration/computer.rb
+++ tictactoe/second_iteration/computer.rb
@@ -42,23 +42,23 @@ class Computer
       puts
       return best_move
     else
-      if b.scenario_one_two?
+      if b.scenario_one_two? && b.list[4] == marker
         best_move = [1, 3, 5, 7].sample
-      elsif b.scenario_three?
+      elsif b.scenario_three? && b.list[4] == marker
         best_move = [7, 8].sample
-      elsif b.scenario_four?
+      elsif b.scenario_four? && b.list[4] == marker
         best_move = [1, 2].sample
-      elsif b.scenario_five?
+      elsif b.scenario_five? && b.list[4] == marker
         best_move = [6, 7].sample
-      elsif b.scenario_six?
+      elsif b.scenario_six? && b.list[4] == marker
         best_move = [0, 1].sample
-      elsif b.scenario_seven?
+      elsif b.scenario_seven? && b.list[4] == marker
         best_move = 6
-      elsif b.scenario_eight?
+      elsif b.scenario_eight? && b.list[4] == marker
         best_move = 8
-      elsif b.scenario_nine?
+      elsif b.scenario_nine? && b.list[4] == marker
         best_move = 0
-      elsif b.scenario_ten?
+      elsif b.scenario_ten? && b.list[4] == marker
         best_move = 2
       else 
         if b.available_corners.size > 0
