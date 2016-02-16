77ba5652e05f4cb60273a945aa156ae5bdab9d42
Michael Aldaba <maldaba3@gmail.com>
2016-02-15 22:22:02 -0500

    Add before hook to board_spec.rb

--- tictactoe/third_iteration/spec/board_spec.rb
+++ tictactoe/third_iteration/spec/board_spec.rb
@@ -1,13 +1,18 @@
 require 'spec_helper'
 
-describe "#size" do 
-  it "size should be :size if :size is initiated" do
-    board = Board.new(:size => 4)
-    expect(board.size).to eq 4
+describe Board do
+  before :each do
+    @board = Board.new
   end
 
-  it "size should be 3 if :size isn't initiated" do
-    board = Board.new
-    expect(board.size).to eq 3
+  describe "#size" do 
+    it "size should be :size if :size is an argument" do
+      @board = Board.new(:size => 4)
+      expect(@board.size).to eq 4
+    end
+
+    it "size should be 3 if :size isn't an argument" do
+      expect(@board.size).to eq 3
+    end
   end
 end
